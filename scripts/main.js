8c60d933282cf7429dec77ce6139fd7f0c75a75a
Tim Dettinger <6506703@isd535.org>
2022-02-10 11:34:19 -0600

    second paddle

--- scripts/main.js
+++ scripts/main.js
@@ -10,19 +10,27 @@ let paddleHeight = 75;
 let paddleWidth = 10;
 let paddleX = (canvas.height - paddleHeight) / 2;
 
-function drawPaddle() {
+function drawPaddle1() {
     ctx.beginPath();
     ctx.rect(canvas.width - paddleWidth, paddleX, paddleWidth, paddleHeight);
     ctx.strokeStyle = "#0095DD";
     ctx.stroke();
     ctx.closePath();
 }
+function drawPaddle2() {
+    ctx.beginPath();
+    ctx.rect(0, paddleX, paddleWidth, paddleHeight);
+    ctx.strokeStyle = "#0095DD";
+    ctx.stroke();
+    ctx.closePath();
+}
+
 
 function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawPaddle();
+    drawPaddle1();
 }
 
 let interval = setInterval(draw, 10);
