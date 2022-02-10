ff77169547160baa0d8119ce5400e3a557e55d8c
Tim Dettinger <6506703@isd535.org>
2022-02-10 11:18:16 -0600

    added canvas and started paddle

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
 // Put your JavaScript here
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d")
+
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+let paddleHeight = 75;
+let paddleWidth = 10;
+let paddleX = (canvas.height - paddleHeight) / 2;
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.width - paddleWidth, paddleHeight, paddleWidth);
+    ctx.strokeStyle = "rgba(0, 0, 225, 0.5)";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function draw() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawPaddle();
+}
+
+let interval = setInterval(draw, 10);
