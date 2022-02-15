1ac93d7ead90bb268333ede19fdfbc731d1ca857
Tim Dettinger <6506703@isd535.org>
2022-02-15 11:34:26 -0600

    timer

--- scripts/main.js
+++ scripts/main.js
@@ -55,9 +55,9 @@ function draw() {
 
     drawBall();
 
-    speed();{
-        window.setTimeout(10000);
-    }
+   
+
+    speed();
 
     // paddle controls
     if (UpPressed) {
@@ -113,6 +113,7 @@ function draw() {
         
         } else {
             alert("GAME OVER");
+            stopTimer();
             document.location.reload();
             clearInterval(interval);
         }
@@ -155,3 +156,62 @@ document.addEventListener("keyup", keyUpHandler, false);
 let interval = setInterval(draw, 10);
 
 
+document.getElementById('stopwatch');
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+    if (stoptime == true) {
+        stoptime = false;
+        timerCycle
+    }
+}
+function stopTimer() {
+    if (stoptime == false) {
+        stoptime = true;
+    }
+}
+function timerCycle() {
+    if (stoptime == false) {
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+
+        sec = sec + 1;
+
+        if (sec == 60) {
+        min = min + 1;
+        sec = 0;
+        }
+        if (min == 60) {
+        hr = hr + 1;
+        min = 0;
+        sec = 0;
+        }
+
+        if (sec < 10 || sec == 0) {
+        sec = '0' + sec;
+        }
+        if (min < 10 || min == 0) {
+        min = '0' + min;
+        }
+        if (hr < 10 || hr == 0) {
+        hr = '0' + hr;
+        }
+
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+        setTimeout("timerCycle()", 1000);
+    }
+}
+
+function resetTimer() {
+    timer.innerHTML = "00:00:00";
+    stoptime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
+}
+startTimer();
