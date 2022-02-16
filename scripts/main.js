f59b28ed9cc676baa77f30e48ca9cef447d9ac96
Tim Dettinger <6506703@isd535.org>
2022-02-16 12:01:26 -0600

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -55,10 +55,8 @@ function draw() {
 
     drawBall();
 
+    score();
    
-
-    speed();
-
     // paddle controls
     if (UpPressed) {
         paddleX -= 7;
@@ -113,7 +111,6 @@ function draw() {
         
         } else {
             alert("GAME OVER");
-            stopTimer();
             document.location.reload();
             clearInterval(interval);
         }
@@ -156,62 +153,7 @@ document.addEventListener("keyup", keyUpHandler, false);
 let interval = setInterval(draw, 10);
 
 
-document.getElementById('stopwatch');
-
-var hr = 0;
-var min = 0;
-var sec = 0;
-var stoptime = true;
-
-function startTimer() {
-    if (stoptime == true) {
-        stoptime = false;
-        timerCycle
-    }
+function score() {
+    setInterval(1000);
+    stopwatch += 1;
 }
-function stopTimer() {
-    if (stoptime == false) {
-        stoptime = true;
-    }
-}
-function timerCycle() {
-    if (stoptime == false) {
-        sec = parseInt(sec);
-        min = parseInt(min);
-        hr = parseInt(hr);
-
-        sec = sec + 1;
-
-        if (sec == 60) {
-        min = min + 1;
-        sec = 0;
-        }
-        if (min == 60) {
-        hr = hr + 1;
-        min = 0;
-        sec = 0;
-        }
-
-        if (sec < 10 || sec == 0) {
-        sec = '0' + sec;
-        }
-        if (min < 10 || min == 0) {
-        min = '0' + min;
-        }
-        if (hr < 10 || hr == 0) {
-        hr = '0' + hr;
-        }
-
-        timer.innerHTML = hr + ':' + min + ':' + sec;
-        setTimeout("timerCycle()", 1000);
-    }
-}
-
-function resetTimer() {
-    timer.innerHTML = "00:00:00";
-    stoptime = true;
-    hr = 0;
-    sec = 0;
-    min = 0;
-}
-startTimer();
